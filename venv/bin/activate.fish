faa8edf3bd8de4b110db288f72011606cfa6f489
virtuooso <virtuooso420@gmail.com>
2022-12-17 17:48:58 +0500

    fixes

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/virtuooso/Develop/basementbot/venv"
+set -gx VIRTUAL_ENV "/home/virtuooso/Develop/community-bot/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
