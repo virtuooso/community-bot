2a32383eda95f935fa16f8360269aabddcd15eb1
virtuooso <117824106+virtuooso@users.noreply.github.com>
2022-12-17 15:41:01 +0500

    Update information.py

--- handlers/users/information.py
+++ handlers/users/information.py
@@ -6,4 +6,4 @@ from data.texts import *
 @dp.callback_query_handler()
 async def information(callback_query: types.CallbackQuery):
     await dp.bot.send_message(chat_id=callback_query.message.chat.id, \
-        text=TEXT_ru if callback_query.data == 'ru' else TEXT_kz, reply_markup=channelMarkup)
+        text=TEXT_ru if callback_query.data == 'ru' else TEXT_kz, reply_markup=channelMarkup, parse_mode='MarkdownV2')
